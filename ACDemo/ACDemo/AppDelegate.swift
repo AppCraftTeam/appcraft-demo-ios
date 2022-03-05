b217890c419be5e50e80f430c5d49c0699f0337a
Dmitriy Polyakov <dmitriyap11@gmail.com>
2022-03-05 16:30:42 +0300

    Create project, add depencices

---
+++ ACDemo/ACDemo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ACDemo
+//
+//  Created by Дмитрий Поляков on 05.03.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
